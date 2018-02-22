5129ba26112421df3225b7893197021ba4809b01
Deyka Howe <deyka.howe@gmail.com>
2018-02-22 15:49:25 -0800

    new User model added to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/index'
-
-  get 'favorites/show'
-
-  get 'favorites/new'
-
-  get 'favorites/create'
-
-  get 'favorites/edit'
-
-  get 'favorites/update'
-
-  get 'favorites/destroy'
-
   root 'users#home'
   resources :users
   resources :favorites

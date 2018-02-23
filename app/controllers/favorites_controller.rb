d5460cf298763a5210c2ba115fbe7fa99acb663a
Deyka Howe <deyka.howe@gmail.com>
2018-02-22 17:17:52 -0800

    add code to the controllers files

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,25 @@
 class FavoritesController < ApplicationController
+  
+  def home
+    redirect_to new_session_path unless logged_in? 
+  end
+  
   def index
+    @favorites = Favorite.all
   end
 
   def show
   end
 
   def new
+    @favorite = Favorite.new 
   end
 
   def create
+    @favorite = Favorite.new(favorite_params)
+    @favorite.user = current_user
+    @favorite.save
+      redirect_to favorites_path
   end
 
   def edit
@@ -18,5 +29,17 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    @favorite = favorite.find(params[:id])
+    if @favorite.destroy
+      redirect_to root_path
+      flash[:alert] = "Favorite deleted"
+    end
+  end
+
+  private
+  def favorite_params
+    params.require(:favorite).permit(:title, :description, :url)
   end
 end
+
+
